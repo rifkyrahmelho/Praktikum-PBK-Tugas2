497f480ae9954e2febf065429d6bae25059fbe47
candra nababan <candranababan95@gmail.com>
2024-05-03 12:43:49 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { reactive, ref } from 'vue'
+
+const awesome = ref(true) 
+const count = ref(1)
+const text = ref('')
+
+
+function increment(){
+  count.value++
+}
+
+function towel() {
+  awesome.value = !awesome.value
+}
+
+
+</script>
+
+<template>
+  <input v-model="text" placeholder="type here">
+    <p>{{ text }}</p>
+      <button @click="towel">towel</button>
+      <h4>
+      <button @click="increment">token: {{ count }}</button>
+      </h4>
+    <h1 v-if="awesome">saya mengaku menang</h1>
+    <h1 v-else>saya mengaku kalah</h1>
+</template>
+
+
